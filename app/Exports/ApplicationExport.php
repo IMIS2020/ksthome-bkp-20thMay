fd37e6eb2ef60fa41a5d4edde85d78e7d7d0bc6d
rxhack <rxhack.contact@gmail.com>
2021-05-13 16:40:11 +0530

    update

--- app/Exports/ApplicationExport.php
+++ app/Exports/ApplicationExport.php
@@ -116,9 +116,9 @@ class ApplicationExport implements
 
     private function instituteData($appId)
     {
-        dd($appId);
+        // dd($appId);
         $hasApplication = ApplicationDetails::where('id',$appId)->first()->hasAdmissionLetter;
-
+        $dd($hasApplication);
         if($hasApplication == 'NO')
         {
             $getInstitute = AnnexureI::with('get_institute')->where('applicationId',$appId)->get();
